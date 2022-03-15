d2d91d4a553884547f943541fea9a72537c4d691
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2022-03-15 02:11:35 -0300

    build: scaffolding

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e813b03853b6917e5cfb4850bd211d3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fdb3ce1e-d477-4fc5-ad14-8c5b5ca05c0f'
+};
